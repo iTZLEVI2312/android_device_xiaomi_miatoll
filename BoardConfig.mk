e67c7cf784b25fa2bc324eb029a4d106c1711b53
Alexander Winkowski <dereference23@outlook.com>
2022-08-06 08:45:07 +0000

    miatoll: Update blobs from V13.0.2.0.SJWMIXM
    Change-Id: I6e0f13aad6116947d5d1d1056f906dfe69cc06ab
    
    Signed-off-by: itzuday2312 <udaybindal2312@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,7 +195,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/rootdir/releasetools
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-06-01
+VENDOR_SECURITY_PATCH := 2022-06-05
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
