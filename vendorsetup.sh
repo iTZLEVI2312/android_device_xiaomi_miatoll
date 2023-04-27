b33e0f211c2a9a3aa90a1da6ece9773701d84cfd
itzuday2312 <udaybindal2312@gmail.com>
2023-04-27 20:41:39 +0530

    miatoll: add vendorsetup.sh for cloning sources
    Signed-off-by: itzuday2312 <udaybindal2312@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Script to Clone Related Sources
+
+# Clone Vendor 
+git clone --depth=1 https://github.com/iTZUDAY2312/android_vendor_xiaomi_miatoll -b 13T vendor/xiaomi/miatoll
+
+# Clone Kernel
+git clone --depth=1 https://github.com/iTZUDAY2312/eletron_kernel_xiaomi_miatoll -b 13T kernel/xiaomi/miatoll 
+
+# Clone proton clang
+git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-proton
+
+# Replace libhidl
+rm -rf system/libhidl && git clone https://github.com/LineageOS/android_system_libhidl -b lineage-20.0 system/libhidl
+
